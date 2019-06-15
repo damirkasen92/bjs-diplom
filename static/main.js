247c5e9c229d06fc44c5a16e7a09c0ee04f9ec2b
damirkasen92 <damir.kasen92@gmail.com>
2019-06-15 18:08:37 +0600

    Диплом

---
+++ static/main.js
@@ -0,0 +1,139 @@
+class Profile {
+  constructor({
+    username,
+    name: { firstName, lastName },
+    password,
+  }) {
+    this.username = username;
+    this.name = { firstName, lastName };
+    this.password = password; 
+  }
+  
+  createUser(callback) {
+    return ApiConnector.createUser({username: this.username, name: this.name, password: this.password}, (err, data) => {
+      console.log(`Создание нового пользователя ${this.username}`);
+      callback(err, data);
+    });
+  }
+  
+  performLogin(callback) {
+    return ApiConnector.performLogin({username: this.username, password: this.password}, (err, data) => {
+      console.log(`Авторизация пользователя ${this.username}`);  
+      callback(err, data);
+    });
+  }  
+  
+  addMoney({currency, amount}, callback) {
+    return ApiConnector.addMoney({currency, amount}, (err, data) => {
+      console.log(`Adding ${amount} of ${currency} to ${this.username}`);  
+      callback(err, data);
+    });  
+  }
+  
+  convertMoney({ fromCurrency, targetCurrency, targetAmount }, callback) {
+    return ApiConnector.convertMoney({ fromCurrency, targetCurrency, targetAmount }, (err, data) => {
+      console.log(`From ${fromCurrency} to ${targetCurrency}`);  
+      callback(err, data);
+    });   
+  }
+  
+  transferMoney({ to, amount }, callback) {
+    return ApiConnector.transferMoney({ to, amount }, (err, data) => {
+      console.log(`Transfering ${amount} of money to ${to}`);
+      callback(err, data);
+    });
+  }
+}
+  
+function getStocks(callback) {
+  return ApiConnector.getStocks((err, data) => {
+    console.log(`Getting stocks info`);  
+    callback(err, data);
+  });   
+}
+  
+function main() {
+  
+  const Petya = new Profile({
+    username: 'Petya',
+    name: {
+      firstName: 'Petya',
+      lastName: 'Petrov'  
+    },
+    password: 'www'
+  });
+  
+  const Vasya = new Profile({
+    username: 'Vasya',
+    name: {
+      firstName: 'Vasya',
+      lastName: 'Vasyatka'  
+    },
+    password: 'sss'
+  });
+    
+  getStocks((err,data) => {
+    if (err) {
+      console.log(`getStocks fail`);  
+    } 
+
+    const convertNow = data[99];
+    
+    Petya.createUser((err, data) => {
+      if (err) {
+        console.log(`Ошибка при создании пользователя`);  
+      } else {
+        console.log(`${Petya.username} создано`);  
+  
+        Petya.performLogin((err, data) => {
+          if (err) {
+            console.log(`Ошибка при авторизации пользователя`);    
+          } else {
+            console.log(`Пользователь авторизован`);  
+  
+            Petya.addMoney({currency: 'EUR', amount: 50000}, (err, data) => {
+              if (err) {
+                console.log(`Ошибка при добавлении денег в кошелек`);    
+              } else {
+                console.log(`Добавлено ${amount} ${currency}`);
+
+                Petya.convertMoney({
+                  fromCurrency: 'EUR',
+                  targetCurrency: 'EUR_NETCOIN', 
+                  targetAmount: 50000 * convertNow.EUR_NETCOIN
+                },
+                (err, data) => {
+                  if (err) {
+                    console.log(`Convert error`);  
+                  } else {
+                    console.log(`Convert success 50000 to ${targetAmount}`);    
+
+                    Vasya.createUser((err, data) => {
+                      if (err) {
+                        console.log(`Ошибка при создании пользователя`);    
+                      } else {
+                        console.log(`${Vasya.username} создано`);  
+
+                        Petya.transferMoney({to: Vasya.username, amount: targetAmount}, (err, data) => {
+                          if (err) {
+                            console.log(`Transfer failed`);  
+                          } else {
+                            console.log(`Transfer success to ${Vasya.username}`);  
+                          }
+                        });
+                      }  
+                    });
+                  }
+                });
+              }
+            });
+          }  
+        });
+      }
+    });  
+  
+  });
+
+}
+  
+main();
